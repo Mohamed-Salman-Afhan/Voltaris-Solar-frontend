2cea7b18f55be08354d1ea2b05a879497167dcaf
Mohamed-Salman-Afhan <salmanafhan59@gmail.com>
2026-01-11 23:03:17 +0530

    updated the Anomalies page to be mobile-responsive

--- src/pages/anomalies/components/EnergyProductionCards.jsx
+++ src/pages/anomalies/components/EnergyProductionCards.jsx
@@ -10,7 +10,7 @@ const EnergyProductionCards = (props) => {
   }
 
   return (
-    <div className="mt-4 grid grid-cols-7 gap-4">
+    <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-4">
       {props.energyProductionData.map((el) => {
         return (
           <EnergyProductionCard
